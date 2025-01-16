926f1ea4599b1c97e682a9847d6816bf0b61c655
Bilal0910 <bilalnadeem4u@gmail.com>
2025-01-17 01:39:00 +0500

    host commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://thrift-store-group-9-ae49a0738ba3.herokuapp.com/api/products/allproducts";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://thrift-store-group-9-ae49a0738ba3.herokuapp.com/api/products/allproducts",
+      "/uploads/": "https://thrift-store-group-9-ae49a0738ba3.herokuapp.com/api/products/uploads",
     },
   },
 });
